d9b999bbc501cb000e72fe33d0c0f72de5b97fe7
Gregory Ostanin <ostingreg@gmail.com>
2020-08-25 08:56:09 -0700

    Initial lights methods

--- home_automation/controllers/lights.py
+++ home_automation/controllers/lights.py
@@ -1,3 +1,37 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 
 lights = Blueprint('lights', __name__)
+
+
+@lights.route('/', methods=['GET'], strict_slashes=False)
+def get_lights():
+    pass
+
+
+@lights.route('/<int:id>', methods=['GET'], strict_slashes=False)
+def get_light(id):
+    pass
+
+
+@lights.route('/', methods=['POST'], strict_slashes=False)
+def save_light():
+    pass
+
+
+@lights.route('/<int:id>', methods=['DELETE'], strict_slashes=False)
+def remove_light(id):
+    pass
+
+
+# TO-DO switch instead of on/off
+@lights.route('/<int:id>/on', methods=['PUT'], strict_slashes=False)
+def on_light(id):
+    pass
+
+
+@lights.route('/<int:id/off', methods=['PUT'], strict_slashes=False)
+def off_light(id):
+    pass
+
+
+# TO-DO turning all lights off and on
