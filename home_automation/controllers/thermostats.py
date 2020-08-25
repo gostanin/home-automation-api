2f67ee45872b51a352dd10ad71d1a10264e3176f
Gregory Ostanin <ostingreg@gmail.com>
2020-08-25 08:56:50 -0700

    Initial methods thermostats

--- home_automation/controllers/thermostats.py
+++ home_automation/controllers/thermostats.py
@@ -1,3 +1,28 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 
 thermostats = Blueprint('thermostats', __name__)
+
+
+@thermostats.route('/', methods=['GET'], strict_slashes=False)
+def get_thermostats():
+    pass
+
+
+@thermostats.route('/<int:id>', methods=['GET'], strict_slashes=False)
+def get_thermostat(id):
+    pass
+
+
+@thermostats.route('/', methods=['POST'], strict_slashes=False)
+def save_thermostat():
+    pass
+
+
+@thermostats.route('/<int:id>', methods=['DELETE'], strict_slashes=False)
+def remove_thermostat(id):
+    pass
+
+
+@thermostats.route('/<int:id>', methods=['PUT'], strict_slashes=False)
+def update_thermostat():
+    pass
